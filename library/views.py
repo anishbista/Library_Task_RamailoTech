725e7e17ee7badae59964a8bceb42f10aeb2ae7e
anishbista92 <anishbista9235@gmail.com>
2024-01-30 17:47:35 +0545

    Api completed

--- library/views.py
+++ library/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User, Book, BookDetails, BorrowedBooks
+from .serializers import (
+    UserSerializer,
+    BookSerializer,
+    BookDetailsSerializer,
+    BorrowedBooksSerializer,
+)
 
-# Create your views here.
+
+# Api for user
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ListUsersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GetUserView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "id"
+
+
+# Api for books
+class AddBookView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class ListBooksView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class GetBookView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "id"
+
+
+class BookDetailsView(generics.CreateAPIView):
+    queryset = BookDetails.objects.all()
+    serializer_class = BookDetailsSerializer
+
+
+# Api for borrowing
+
+
+class BorrowBookView(generics.CreateAPIView):
+    queryset = BorrowedBooks.objects.all()
+    serializer_class = BorrowedBooksSerializer
+
+
+class ReturnBookView(generics.UpdateAPIView):
+    queryset = BorrowedBooks.objects.all()
+    serializer_class = BorrowedBooksSerializer
+    lookup_field = "id"
+
+
+class ListBorrowedBooksView(generics.ListAPIView):
+    queryset = BorrowedBooks.objects.all()
+    serializer_class = BorrowedBooksSerializer
